0885e058d053c1850dc68190ed6293da7c1e67f1
Sheogorattio <no.passion.there.is.serenity@gmail.com>
2022-11-09 04:16:19 +0200

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,86 @@
+#include<windows.h>
+#include<tchar.h>
+#include"resource.h"
+
+BOOL CALLBACK DlgProc(HWND, UINT, WPARAM, LPARAM);
+
+HWND hBoxArr[11], hRadioArr[24], hPicture;
+
+HBITMAP hBmp;
+
+int	WINAPI _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstace, LPTSTR lpszCmdLine, int nCmdShow) {
+	return DialogBox(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), 0, (DLGPROC)DlgProc);
+}
+
+BOOL CALLBACK DlgProc(HWND hWnd, UINT uMessage, WPARAM wParam, LPARAM lParam) {
+	switch (uMessage) {
+		case WM_INITDIALOG:
+			for (int i = 0; i < 11; i++) {
+				hBoxArr[i] = GetDlgItem(hWnd, IDC_CHECK1 + i);
+			}
+			hPicture = GetDlgItem(hWnd, IDC_PICTURE);
+			hBmp = LoadBitmap(GetModuleHandle(NULL), MAKEINTRESOURCE(IDB_BITMAP1));
+			SendMessage(hPicture, STM_SETIMAGE, 0, LPARAM(hBmp));
+			return TRUE;
+		
+		case WM_CLOSE:
+			EndDialog(hWnd,0);
+			return TRUE;
+		
+		case WM_COMMAND:
+		{
+			if (LOWORD(wParam) == IDOK) {
+				INT nAnswersRadio[7] = {IDC_RADIO1, IDC_RADIO5, IDC_RADIO8, IDC_RADIO12, IDC_RADIO14, IDC_RADIO16, IDC_RADIO22};
+				INT nAnswersCheckB1[2]= { IDC_CHECK1, IDC_CHECK3};
+				INT nAnswersCheckB2[3] = { IDC_CHECK5, IDC_CHECK6, IDC_CHECK8 };
+				INT nAnswersCheckB3[3] = { IDC_CHECK9, IDC_CHECK10, IDC_CHECK11 };
+				INT nMaxMark = 10, nErrors = nMaxMark;
+				for (int i = 0; i < 7; i++) {
+					if (BST_CHECKED == SendDlgItemMessage(hWnd, nAnswersRadio[i], BM_GETCHECK, 0, 0)) nErrors--;
+				}
+				INT CheckBoxPoints = 0;
+				for (int i = 0; i < 4; i++) {//первый блок
+					if (IDC_CHECK1 + i == nAnswersCheckB1[0] || IDC_CHECK1 + i == nAnswersCheckB1[1]) {
+						if (BST_CHECKED == SendMessage(hBoxArr[i], BM_GETCHECK, 0, 0)) CheckBoxPoints++;
+					}
+					if (CheckBoxPoints == 2) {
+						nErrors--;
+						
+						break;
+					}
+				}
+				CheckBoxPoints = 0;
+				for (int i = 4; i < 8; i++) {//второй блок
+					if (IDC_CHECK1 + i == nAnswersCheckB2[0] || IDC_CHECK1 + i == nAnswersCheckB2[1] || IDC_CHECK1 + i == nAnswersCheckB2[2]) {
+						if (BST_CHECKED == SendMessage(hBoxArr[i], BM_GETCHECK, 0,0)) CheckBoxPoints++;
+					}
+					if (CheckBoxPoints == 3) {
+						nErrors--;
+						
+						break;
+					}
+				}
+				CheckBoxPoints = 0;
+				for (int i = 8; i < 11; i++) {//третий блок
+					if (IDC_CHECK1 + i == nAnswersCheckB3[0] || IDC_CHECK1 + i == nAnswersCheckB3[1] || IDC_CHECK1 + i == nAnswersCheckB3[2]) {
+						if (BST_CHECKED == SendMessage(hBoxArr[i], BM_GETCHECK, 0, 0)) CheckBoxPoints++;
+					}
+					if (CheckBoxPoints == 3) {
+						nErrors--;
+						
+						break;
+					}
+				}
+				CheckBoxPoints = 0;
+				INT mark = nMaxMark - nErrors;
+				DOUBLE result = (double)mark/nMaxMark*100;
+				
+				TCHAR str[50];
+				_stprintf_s(str, TEXT("Ваш результат %d. Точність %f %%"), mark, result);
+				MessageBox(hWnd, str, TEXT("Result"), MB_OK);
+			}
+			return TRUE;
+		}
+	}
+	return FALSE;
+}
---
+++ resource.h
@@ -0,0 +1,53 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by Quiz.rc
+//
+#define IDD_DIALOG1                     101
+#define IDB_BITMAP1                     104
+#define IDC_RADIO1                      1001
+#define IDC_RADIO2                      1002
+#define IDC_RADIO3                      1003
+#define IDC_RADIO4                      1004
+#define IDC_RADIO5                      1005
+#define IDC_RADIO6                      1006
+#define IDC_RADIO7                      1007
+#define IDC_RADIO8                      1008
+#define IDC_RADIO9                      1009
+#define IDC_RADIO10                     1010
+#define IDC_RADIO11                     1011
+#define IDC_RADIO12                     1012
+#define IDC_RADIO13                     1013
+#define IDC_RADIO14                     1014
+#define IDC_RADIO15                     1015
+#define IDC_RADIO16                     1016
+#define IDC_RADIO17                     1017
+#define IDC_RADIO18                     1018
+#define IDC_RADIO19                     1019
+#define IDC_RADIO20                     1020
+#define IDC_RADIO21                     1021
+#define IDC_RADIO22                     1022
+#define IDC_RADIO23                     1023
+#define IDC_RADIO24                     1024
+#define IDC_CHECK1                      1025
+#define IDC_CHECK2                      1026
+#define IDC_CHECK3                      1027
+#define IDC_CHECK4                      1028
+#define IDC_CHECK5                      1029
+#define IDC_CHECK6                      1030
+#define IDC_CHECK7                      1031
+#define IDC_CHECK8                      1032
+#define IDC_CHECK9                      1033
+#define IDC_CHECK10                     1034
+#define IDC_CHECK11                     1035
+#define IDC_PICTURE                     1040
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        105
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1036
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
